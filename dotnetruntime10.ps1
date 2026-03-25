7439b8b04751f1a7456b0c4cad8707ad90cb815a
T3-Steve <steve.wilson@quo.cc>
2026-03-25 17:34:27 -0400

    Refactor .NET runtime installation in dotnetruntime10.ps1
    Updated script to use Install-WinGetPackage for .NET runtime installation and added package management commands.

--- dotnetruntime10.ps1
+++ dotnetruntime10.ps1
@@ -1,3 +1,9 @@
 cd C:\Program Files\NinjaRemote\git
 import-module git-posh
-winget install microsoft.dotnet.runtime.10 -force
+Install-PackageProvider -Name NuGet -Force
+Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGa
+llery
+Find-WinGetPackage -Query "Microsoft.dotnet.Runtime.10" | Install-WinGetPackage
+Get-WinGetPackage | Where-Object { $_.IsUpdateAvailable } | Update-WinGetPackage
+ 
+#Install-WinGetPackage -Id  microsoft.dotnet.runtime.10 -Source WinGet
