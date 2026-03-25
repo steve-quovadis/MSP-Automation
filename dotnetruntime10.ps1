1765efe1d890fcaaa813f0423d511a95ee9be8de
T3-Steve <steve.wilson@quo.cc>
2026-03-25 17:38:48 -0400

    Fix module import and update package installation commands

--- dotnetruntime10.ps1
+++ dotnetruntime10.ps1
@@ -1,8 +1,7 @@
 cd C:\Program Files\NinjaRemote\git
-import-module git-posh
+Import-Module posh-git
 Install-PackageProvider -Name NuGet -Force
-Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGa
-llery
+Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery
 Find-WinGetPackage -Query "Microsoft.dotnet.Runtime.10" | Install-WinGetPackage
 Get-WinGetPackage | Where-Object { $_.IsUpdateAvailable } | Update-WinGetPackage
  
